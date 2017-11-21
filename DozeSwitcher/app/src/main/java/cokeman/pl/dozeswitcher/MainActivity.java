07caaee95d33bfefdf2073dec3fc1479216c9b40
Maciej Czapla <maciekczapla@gmail.com>
2017-11-21 20:44:23 +0100

    Improve quality of code

--- DozeSwitcher/app/src/main/java/cokeman/pl/dozeswitcher/MainActivity.java
+++ DozeSwitcher/app/src/main/java/cokeman/pl/dozeswitcher/MainActivity.java
@@ -1,10 +1,13 @@
 package cokeman.pl.dozeswitcher;
 
 import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -15,21 +18,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
+import static android.os.Build.VERSION;
+import static android.widget.CompoundButton.OnCheckedChangeListener;
+import static cokeman.pl.dozeswitcher.Commands.COMMAND_DUMPSYS_DISABLE;
+import static cokeman.pl.dozeswitcher.Commands.COMMAND_DUMPSYS_ENABLE;
+import static cokeman.pl.dozeswitcher.Commands.COMMAND_DUMPSYS_GREP_ABOVE_M;
+import static cokeman.pl.dozeswitcher.Commands.COMMAND_DUMPSYS_GREP_M;
+import static cokeman.pl.dozeswitcher.Commands.COMMAND_SU;
+
 public class MainActivity extends AppCompatActivity {
     private Switch dozeSwitch;
-    private TextView dozeReader;
-    private TextView sdkVersionNumber;
+    private TextView dozeReaderTv;
+    private TextView sdkVersionNumberTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dozeSwitch = (Switch) findViewById(R.id.switch1);
-        dozeReader = (TextView) findViewById(R.id.dozeReader);
-        sdkVersionNumber = (TextView) findViewById(R.id.textView3);
 
-        startupStatus();
-        dozeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        initControls();
+        setStartValues();
+        setListeners();
+    }
+
+    private void initControls() {
+        dozeSwitch = (Switch) findViewById(R.id.switch1);
+        dozeReaderTv = (TextView) findViewById(R.id.dozeReader_tv);
+        sdkVersionNumberTv = (TextView) findViewById(R.id.textView);
+    }
+
+    private void setListeners() {
+        dozeSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton cb, boolean on) {
                 switchDoze(on);
@@ -37,110 +56,120 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void startupStatus() {
-        dozeSwitch.setChecked(checkDozeStatus());
-        changeDozeText(checkDozeStatus());
+    private void setStartValues() {
+        boolean status = checkDozeStatus();
+        dozeSwitch.setChecked(status);
+        changeDozeText(status);
     }
 
-    private Boolean checkDozeStatus() {
+    private boolean checkDozeStatus() {
         try {
             Process process = Runtime.getRuntime().exec("su");
-            OutputStream out = process.getOutputStream();
-            if (getSDKVersionNumber() > 23) {
-                out.write("dumpsys deviceidle | grep mDeepEnabled\n".getBytes());
-                if (getSDKVersionNumber() < 26) {
-                    sdkVersionNumber.setText("Nougat");
-                } else {
-                    sdkVersionNumber.setText("Oreo");
-                }
-            } else if (getSDKVersionNumber() == 23) {
-                sdkVersionNumber.setText("Marshmallow");
-                out.write("dumpsys deviceidle | grep mEnabled\n".getBytes());
-            }
-            out.close();
-            StringBuffer output = getStringBuffer(process);
-            String s = output.toString();
-            if (s.contains("true")) {
+            OutputStream outputStream = process.getOutputStream();
+
+            setVersionName();
+
+            outputStream.write(getCommandBaseOnVersion(VERSION.SDK_INT));
+            outputStream.close();
+
+            String output = getStringBuilderOutput(process);
+
+            if (output.contains("true")) {
                 return true;
-            } else if (s.equals("")) {
-                noRootInfo();
-                return false;
-            } else {
-                return false;
+            } else if (output.equals("")) {
+                noRootInfoDialog();
             }
+
         } catch (IOException e) {
-            noRootInfo();
-            return false;
+            noRootInfoDialog();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+
+        return false;
+    }
+
+    private byte[] getCommandBaseOnVersion(int apiVersion) {
+        if (apiVersion > VERSION_CODES.M) {
+            return COMMAND_DUMPSYS_GREP_ABOVE_M.getBytes();
+        } else if (apiVersion == VERSION_CODES.M) {
+            return COMMAND_DUMPSYS_GREP_M.getBytes();
+        }
+        return "".getBytes();
+    }
+
+    private void setVersionName() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            sdkVersionNumberTv.setText(packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
     @NonNull
-    private StringBuffer getStringBuffer(Process process) throws IOException, InterruptedException {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(process.getInputStream()));
+    private String getStringBuilderOutput(Process process) throws IOException, InterruptedException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
         int read;
         char[] buffer = new char[4096];
-        StringBuffer output = new StringBuffer();
+        StringBuilder output = new StringBuilder();
         while ((read = reader.read(buffer)) > 0) {
             output.append(buffer, 0, read);
         }
         reader.close();
         process.waitFor();
-        return output;
+        return output.toString();
     }
 
-    private void noRootInfo() {
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-        builder1.setTitle("Missing root");
-        builder1.setMessage("Please root your device first");
-        builder1.setCancelable(false);
-        builder1.setNeutralButton(android.R.string.ok,
+    private void noRootInfoDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.missing_root_title);
+        builder.setMessage(R.string.missing_root_description);
+        builder.setNeutralButton(android.R.string.ok,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         finish();
                     }
                 });
-        AlertDialog alert11 = builder1.create();
-        alert11.show();
-    }
-
-    private Integer getSDKVersionNumber() {
-        try {
-            Process process = Runtime.getRuntime().exec("getprop ro.build.version.sdk\n");
-            StringBuffer output = getStringBuffer(process);
-            String s = output.toString().replaceAll("\\D", "");
-            return Integer.parseInt(s);
-        } catch (IOException e) {
-            throw new RuntimeException();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     private void switchDoze(boolean on) {
         try {
-            Process process = Runtime.getRuntime().exec("su");
+            Process process = Runtime.getRuntime().exec(COMMAND_SU);
             OutputStream out = process.getOutputStream();
             changeDozeText(on);
+
             if (on) {
-                out.write("dumpsys deviceidle enable\n".getBytes());
-                Toast.makeText(MainActivity.this, "Doze enabled", Toast.LENGTH_SHORT).show();
+                enableDoze(out);
             } else {
-                out.write("dumpsys deviceidle disable\n".getBytes());
-                Toast.makeText(MainActivity.this, "Doze disabled", Toast.LENGTH_SHORT).show();
+                disableDoze(out);
             }
+
             out.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private void disableDoze(OutputStream out) throws IOException {
+        out.write(COMMAND_DUMPSYS_DISABLE.getBytes());
+        showToast(getString(R.string.doze_disabled));
+    }
+
+    private void enableDoze(OutputStream out) throws IOException {
+        out.write(COMMAND_DUMPSYS_ENABLE.getBytes());
+        showToast(getString(R.string.doze_enabled));
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+    }
+
     private void changeDozeText(boolean on) {
-        String dozeStatus = on ? "enabled" : "disabled";
-        dozeReader.setText("Doze " +dozeStatus);
+        String dozeStatus = on ? getString(R.string.enabled) : getString(R.string.disabled);
+        dozeReaderTv.setText(String.format(getString(R.string.doze_status), dozeStatus));
     }
 }

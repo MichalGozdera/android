86efc523101a59db50c2528996602285627f2f1c
Michal Gozdera <michael.gozdera@gmail.com>
2018-11-04 21:41:48 +0100

    dziala sledzenie gps

--- gmap/appa/src/main/java/com/example/cokeman/myapplication/MainActivity.java
+++ gmap/appa/src/main/java/com/example/cokeman/myapplication/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.cokeman.myapplication;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
@@ -23,15 +29,21 @@ import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    Location gpsLocation;
+    final GeoPoint misio = new GeoPoint(0, 0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button gpsButton = (Button) findViewById(R.id.getGps);
-        final TextView textView = (TextView) findViewById(R.id.coordinates);
+        final Button findDorotka = (Button) findViewById(R.id.getGps);
+        final Button gps = (Button) findViewById(R.id.gps);
         final MapView osm = (MapView) findViewById(R.id.osm);
+        final MapController mc = (MapController) osm.getController();
+        final TextView textView = (TextView) findViewById(R.id.coordinates);
+
+        final LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
 
 
         final float scale = this.getResources().getDisplayMetrics().density;
@@ -48,9 +60,9 @@ public class MainActivity extends AppCompatActivity {
         osm.setBuiltInZoomControls(true);
         osm.setMultiTouchControls(true);
 
-        final MapController mc = (MapController) osm.getController();
+
         final GeoPoint dorotka = new GeoPoint(51.0005, 21.7860);
-        gpsButton.setOnClickListener(new Button.OnClickListener() {
+        findDorotka.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 osm.setVisibility(View.VISIBLE);
                 textView.setText("Dorotka jest tutaj:");
@@ -60,8 +72,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // google.setVisibility(View.INVISIBLE);
 
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, this);
+        gps.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                gpsLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                osm.setVisibility(View.VISIBLE);
+                textView.setText("Misio jest tutaj:");
+                misio.setLatitude(gpsLocation.getLatitude());
+                misio.setLongitude(gpsLocation.getLongitude());
+                mc.animateTo(misio);
+                mc.setZoom(16);
+                osm.setTilesScaledToDpi(true);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String provider = location.getProvider();
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        float accuracy = location.getAccuracy();
+        long time = location.getTime();
+        misio.setLatitude(lat);
+        misio.setLongitude(lng);
+        MapView osm = (MapView) findViewById(R.id.osm);
+        MapController mc = (MapController) osm.getController();
+        mc.animateTo(misio);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 }
